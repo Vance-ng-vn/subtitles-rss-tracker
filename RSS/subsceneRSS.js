398a7012b6620edda2ddd9dcfd1ff1214a387a3d
Vance-ng-vn <vance.ng.vn@gmail.com>
2024-01-25 18:39:39 +0700

    first commit

--- RSS/subsceneRSS.js
+++ RSS/subsceneRSS.js
@@ -17,7 +17,7 @@ async function fetchRSS(url) {
 
         if(res?.body) {
             const fetch = JSON.parse(res.body);
-            
+
             if(fetch.length) {
                 //save to db
                 const insert_many = [...fetch].map(item => Object.values(item));
@@ -37,7 +37,7 @@ async function fetchRSS(url) {
 
 
 async function sync() {
-    await fetchRSS(RSS_URL.Subscene.film);
+    await fetchRSS(RSS_URL.Subscene.movie);
     setTimeout(async ()  => await fetchRSS(RSS_URL.Subscene.series), 1*MINUTES);
 }
 
